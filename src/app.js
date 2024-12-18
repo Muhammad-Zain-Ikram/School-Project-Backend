212b108af71211e7a28e43a011ca026eb8b5d226
Zain IKram <gamingsage92@gmail.com>
2024-12-18 12:49:18 +0500

    Here double cors is removed and single is addded Also vercel.json is modify to detect serverless functions

--- src/app.js
+++ src/app.js
@@ -17,20 +17,15 @@ app.options('*', cors());
 
 app.use(
   helmet({
-    contentSecurityPolicy : false
-    // contentSecurityPolicy: {
-    //   directives: {
-    //     defaultSrc: ["'self'"],
-    //     scriptSrc: ["'self'"],
-    //     },
-    //   }
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+        },
+      }
     })
   );
   app.use(helmet.xssFilter({ setOnOldIE: true }));
-  app.use(cors({
-    origin : process.env.CORS_ORIGIN,
-    credentials : true
-  }))
   app.use(express.json({limit : "16kb"}))
   app.use(express.static("public"))
   app.use(cookieParser())
